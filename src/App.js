8959c7fd92072585f82b6d3ca7fc6eb755fb1187
Daniel Samanliev <dansamanliev@gmail.com>
2021-03-05 00:25:40 +0200

    Add component for insight

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import React from "react";
-import {
-  List,
-  AutoSizer,
-  CellMeasurer,
-  CellMeasurerCache,
-} from "react-virtualized";
+import * as React from "react";
 import { loremIpsum, username } from "react-lorem-ipsum";
 import "./App.css";
+import * as ReactDOM from "react-dom";
+import { GroupedVirtuoso } from "react-virtuoso";
+import Insight from "./components/Insight";
 
 export default function App() {
   const [insights, setInsights] = React.useState([]);
-  const cache = React.useRef(
-    new CellMeasurerCache({
-      fixedWidth: true,
-      defaultHeight: 150,
-    })
-  );
 
   React.useEffect(() => {
     setInsights(
-      [...Array(100).keys()].map((key) => {
-        return {
-          id: key,
-          title: username(),
-          content: loremIpsum({
-            p: 1,
-            startWithLoremIpsum: false,
-            random: true,
-          }),
-        };
-      })
+      [...Array(100).keys()]
+        .map((key) => {
+          return {
+            id: key,
+            title: username(),
+            content: loremIpsum({
+              p: 1,
+              startWithLoremIpsum: false,
+              random: true,
+            }),
+            //TODO: Add more groups
+            group: Math.random() * 100 > 50 ? "Key" : "Personal",
+          };
+        })
+        .sort((a, b) => a.group.localeCompare(b.group))
     );
   }, []);
+
+  // TODO: Probably prettier ways of doing this
+  const groups = ["Key", "Personal"];
+  let groupCounts = [];
+
+  groups.forEach((groupName) => {
+    groupCounts.push(
+      insights.reduce(
+        (counter, { group }) =>
+          groupName.localeCompare(group) === 0 ? (counter += 1) : counter,
+        0
+      )
+    );
+  });
 
   return (
     <div className="appBody">
@@ -39,47 +48,46 @@ export default function App() {
         <h1>Accounts</h1>
       </div>
       <div className="appContent">
+        {console.log(groupCounts)}
         <div>&nbsp;</div>
         <div className="insights" style={{ width: "100%", height: "100vh" }}>
-          <AutoSizer>
-            {({ width, height }) => (
-              <List
-                width={width}
-                height={height}
-                rowHeight={cache.current.rowHeight}
-                deferredMeasurementCache={cache.current}
-                rowCount={insights.length}
-                rowRenderer={({ key, index, style, parent }) => {
-                  const insight = insights[index];
-
-                  return (
-                    <CellMeasurer
-                      key={key}
-                      cache={cache.current}
-                      parent={parent}
-                      columnIndex={0}
-                      rowIndex={index}
-                    >
-                      <div key={key} className="insight" style={style}>
-                        <div>
-                          <div className="insightTitle">{insight.title}</div>
-                          <div className="insightContent">
-                            {insight.content}
-                          </div>
-                        </div>
-                        <div className="insightOptions">x</div>
-                      </div>
-                    </CellMeasurer>
-                  );
-                }}
-              />
-            )}
-          </AutoSizer>
+          <GroupedVirtuoso
+            groupCounts={groupCounts}
+            groupContent={(index) => {
+              return (
+                <div
+                  style={{
+                    backgroundColor: "white",
+                    fontSize: "30px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  {groups[index]}
+                </div>
+              );
+            }}
+            itemContent={(index) => {
+              return (
+                <Insight
+                  key={insights[index].id}
+                  group={insights[index].group}
+                  title={insights[index].title}
+                  content={insights[index].content}
+                />
+              );
+            }}
+          />
         </div>
         <div>&nbsp;</div>
       </div>
       <div className="appOptions">
         <h1>Options</h1>
+        <div className="appOptionsGroups">
+          <h1>Groups</h1>
+          {groups.map((value, index) => {
+            return <h3 key={index}>{value}</h3>
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Insight.js
@@ -0,0 +1,45 @@
+const Insight = (props) => {
+  return (
+    <div key={props.key} className="insight">
+      <div>
+        <div className="insightTitle">
+          {props.group} : {props.title}
+        </div>
+        <div className="insightContent">{props.content}</div>
+      </div>
+      <div className="insightOptions">
+        <div>&nbsp;</div>
+        <svg
+          class="cursor-pointer hover:border-green-500 hover:text-green-500 border rounded text-inactive"
+          width="27"
+          height="27"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"></path>
+        </svg>
+        <svg
+          class="cursor-pointer hover:border-red-500 hover:text-red-500 border rounded text-inactive"
+          width="27"
+          height="27"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3"></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Insight;
